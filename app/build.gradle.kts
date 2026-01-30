2081c3b01f548548fc19ac8692113834d5cf144a
explysm <joshualtrost@gmail.com>
2026-01-30 09:20:07 -0600

    fix: Add Material Components dependency to resolve Material3 theme not found error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("com.google.android.material:material:1.11.0") // Added for Material3 themes resolution
 
     // For WebView
     implementation("androidx.webkit:webkit:1.9.0")
