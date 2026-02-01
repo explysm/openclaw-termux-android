9707b7df73fcb1fe76eb30118a30bfe97dc479b6
explysm <97466432+explysm@users.noreply.github.com>
2026-01-31 20:39:50 -0600

    Bump version name to v1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "v1.0.0"
+        versionName = "v1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
